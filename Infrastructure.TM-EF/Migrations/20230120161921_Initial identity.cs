ee8914f10a8b3c03ff4e12dcd8230609fb7482e4
TurboSjaantje <daanvdm@hotmail.com>
2023-01-20 17:24:05 +0100

    migrations

---
+++ Infrastructure.TM-EF/Migrations/20230120161921_Initial identity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.TM_EF.Migrations
+{
+    public partial class Initialidentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
